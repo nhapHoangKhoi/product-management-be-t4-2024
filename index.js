0f29c5a19d26d97eac26d8087af45fb598fd2a17
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2024-09-29 15:28:51 +0700

    Comment response.render(), mac dinh di vao folder views

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const routeClient = require("./routes/client/index.route.js"); // nhúng file in
 const app = express();  // khoi tao ung dung web su dung express
 const port = 3000;
 
-app.set("views", "./views"); // folder nao cung duoc, vd: app.set("views", "./abc")
+app.set("views", "./views"); // doi voi response.render(), mac dinh di vao folder views
 app.set("view engine", "pug");
 
 routeClient.index(app); // goi den ham index cua file index.route.js
