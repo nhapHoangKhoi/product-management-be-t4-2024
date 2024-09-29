c921eb3994a6b7a736f1a8f9103cbaa38e9ae05d
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2024-09-29 16:29:49 +0700

    Nhúng file tĩnh

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const port = process.env.PORT;
 app.set("views", "./views"); // doi voi response.render(), mac dinh di vao folder views
 app.set("view engine", "pug");
 
+app.use(express.static("public")); // nhung cac file tinh, tuc la folder public, mac dinh tu di vao folder public
+
 routeClient.index(app); // goi den ham index cua file index.route.js
 
 
