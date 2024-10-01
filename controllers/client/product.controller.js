98ef95c99844ace25f394bed9df372911b405dfc
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2024-10-01 16:54:26 +0700

    Làm trang danh sách sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,7 +1,22 @@
+const ProductModel = require("../../models/product.model.js");
+
 // [GET] /products/
-module.exports.index = (request, response) => {
+module.exports.index = async (request, response) => 
+{
+   const listOfProducts = await ProductModel.find(
+      {
+         status: "active",
+         deleted: false
+      }
+   );
+
+   for(aProduct of listOfProducts) {
+      aProduct.priceNew = ( aProduct.price - (aProduct.price*aProduct.discountPercentage/100) ).toFixed(0);
+   }
+
    response.render("client/pages/products/index.pug", {
-      pageTitle: "Danh sách sản phẩm"
+      pageTitle: "Danh sách sản phẩm",
+      listOfProducts: listOfProducts
    });
 }
 
