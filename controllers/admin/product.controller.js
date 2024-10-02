fa8e55390aab431f5d68416b2e56e9504e4f7bee
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2024-10-02 16:28:32 +0700

    Tính năng lọc theo trạng thái bên Frontend

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -7,6 +7,9 @@ module.exports.index = async (request, response) =>
       deleted: false
    };
 
+   // {status: "active"}
+   // {status: "inactive"}
+   // {status: undefined}
    if(request.query.status) {
       productFind.status = request.query.status;
    }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,1 +1,41 @@
-console.log("OK admin");
+// Button status
+
+const listButtonStatus = document.querySelectorAll("[button-status]");
+
+if(listButtonStatus.length > 0)
+{
+   // console.log(window.location.href); 
+   // http://localhost:3000/admin333/products
+
+   let newURL = new URL(window.location.href);
+
+   listButtonStatus.forEach((eachButton) => {
+      eachButton.addEventListener("click", () => 
+         {
+            const statusOfButton = eachButton.getAttribute("button-status");
+
+            if(statusOfButton != "") {
+               newURL.searchParams.set("status", statusOfButton);
+            }
+            else {
+               newURL.searchParams.delete("status");
+            }
+
+            // console.log(newURL.href); 
+            // http://localhost:3000/admin333/products?status=active
+
+            window.location.href = newURL.href;
+         }
+      );
+   });
+
+   // add class "active" for button
+   const statusCurrent = newURL.searchParams.get("status") || "";
+   const buttonCurrent = document.querySelector(`[button-status="${statusCurrent}"]`);
+
+   if(buttonCurrent) {
+      buttonCurrent.classList.add("active");
+   }
+}
+
+// End button status
