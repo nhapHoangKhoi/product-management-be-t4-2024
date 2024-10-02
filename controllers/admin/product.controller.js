cec03b14ea35b74a86ef5c1a3d76adfba30849bf
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2024-10-02 16:52:36 +0700

    TÍnh năng tìm kiếm bên Backend

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,6 +3,8 @@ const ProductModel = require("../../models/product.model.js");
 // [GET] /admin/products/
 module.exports.index = async (request, response) => 
 {
+   // Filter by status
+
    const productFind = {
       deleted: false
    };
@@ -14,13 +16,32 @@ module.exports.index = async (request, response) =>
       productFind.status = request.query.status;
    }
 
+   // End filter by status
+
+
+   // Search item
+
+   let keyword = "";
+   
+   if(request.query.inputKeyword) 
+   {
+      const regex = new RegExp(request.query.inputKeyword, "i");
+
+      productFind.title = regex;
+      keyword = request.query.inputKeyword;
+   }
+
+   // End search item
+
+
    const listOfProducts = await ProductModel.find(productFind);
 
    response.render(
       "admin/pages/products/index.pug", 
       {
          pageTitle: "Quản lý sản phẩm",
-         listOfProducts: listOfProducts
+         listOfProducts: listOfProducts,
+         keyword: keyword
       }
    );
 }
