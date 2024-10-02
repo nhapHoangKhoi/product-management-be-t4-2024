b6a343683c881ed38316e6a322808a0faced405a
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2024-10-02 22:04:02 +0700

    Tính năng phân trang bên Backend

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -49,7 +49,32 @@ module.exports.index = async (request, response) =>
    // ----- End search products
 
 
-   const listOfProducts = await ProductModel.find(productFind);
+   // ----- Pagination
+
+   // { currentPage: 1, limitItems: 4, startIndex: 0, totalPage: 5 }
+   const pagination = {
+      currentPage: 1,
+      limitItems: 4
+   };
+
+   if(request.query.page) {
+      pagination.currentPage = parseInt(request.query.page);
+   }
+
+   pagination.startIndex = (pagination.currentPage - 1) * pagination.limitItems;
+
+   const totalProductsCounted = await ProductModel.countDocuments(productFind);
+   const totalPage = Math.ceil(totalProductsCounted / pagination.limitItems);
+   pagination.totalPage = totalPage;
+
+
+   // ----- End pagination
+
+
+   const listOfProducts = await ProductModel
+      .find(productFind)
+      .limit(pagination.limitItems)
+      .skip(pagination.startIndex);
 
    response.render(
       "admin/pages/products/index.pug", 
@@ -57,7 +82,8 @@ module.exports.index = async (request, response) =>
          pageTitle: "Quản lý sản phẩm",
          listOfProducts: listOfProducts,
          keyword: keyword,
-         filterStatusForFE: filterStatusForFE
+         filterStatusForFE: filterStatusForFE,
+         pagination: pagination
       }
    );
 }
