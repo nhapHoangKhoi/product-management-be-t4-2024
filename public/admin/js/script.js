d742dd386df2a407f86207204d5b166033a21812
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2024-10-07 23:14:59 +0700

    Thong bao BE fadeIn mac dinh

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -517,7 +517,7 @@ if(notification)
    let timeExpiredNotification = notification.getAttribute("show-notification") || 3000;
    timeExpiredNotification = parseInt(timeExpiredNotification);
 
-   fadeInFE(notification, 0.5);
+   fadeInFE(notification);
 
    fadeOutBE(notification, timeExpiredNotification);
 }
