6aa6a50069cf1b02e41ffea3ae5b336977166feb
spryzenrequiem <116591477+spryzenrequiem@users.noreply.github.com>
2024-10-09 01:44:06 +0700

    Xây dựng phần Thêm mới nhóm quyền

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,4 +1,5 @@
 const RoleModel = require("../../models/role.model.js");
+const systemConfigs = require("../../config/system.js");
 
 // ----------------[]------------------- //
 // [GET] /admin/roles/
@@ -17,5 +18,30 @@ module.exports.index = async (request, response) =>
          listOfRoles: listOfRoles
       }
    );
+}
+// ----------------End []------------------- //
+
+
+// ----------------[]------------------- //
+// [GET] /admin/roles/create
+module.exports.getCreatePage = (request, response) => 
+{
+   response.render(
+      "admin/pages/roles/create.pug", 
+      {
+         pageTitle: "Tạo mới nhóm quyền",
+      }
+   );
+}
+
+// [POST] /admin/roles/create
+module.exports.createRole = async (request, response) =>
+{
+   const newRoleModel = new RoleModel(request.body);
+   await newRoleModel.save();
+
+   // response.send("OK Frontend");
+   request.flash("success", "Thêm mới nhóm quyền thành công!");
+   response.redirect(`/${systemConfigs.prefixAdmin}/roles`);
 }
 // ----------------End []------------------- //
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -5,4 +5,7 @@ const controllerAdmin = require("../../controllers/admin/role.controller.js");
 
 router.get("/", controllerAdmin.index);
 
+router.get("/create", controllerAdmin.getCreatePage);
+router.post("/create", controllerAdmin.createRole);
+
 module.exports = router;
